15eb714a6d972f671df612c2aff08a72095eacdd
luca <luca.defranceschi.91@gmail.com>
2016-06-24 17:10:37 +0200

    - Added front-end skeleton

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nodeChatroomApp
+ * @description
+ * # nodeChatroomApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nodeChatroomApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodeChatroomApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nodeChatroomApp
+ */
+angular.module('nodeChatroomApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
